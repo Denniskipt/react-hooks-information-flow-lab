7d66b66fcff42442f3bab5168ca0cf301212d61b
+dennis kiptoo <+dennis.kiptoo@student.moringaschool.com>
2022-07-14 23:07:48 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
+import Header from "./Header"
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
+  const[items ,setItems]= useState(itemData)
 
   function handleDarkModeClick() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
@@ -17,7 +19,7 @@ function App() {
           {isDarkMode ? "Dark" : "Light"} Mode
         </button>
       </header>
-      <ShoppingList items={itemData} />
+      <ShoppingList items={items} />
     </div>
   );
 }
